e90176c341eaa13b66fe89b6510b40de2e562ecc
MohammedAldalooj <96469073+mohammedAldalooj-penny@users.noreply.github.com>
2023-09-30 21:08:51 +0300

    update api

--- next-mongo-app/pages/api/getAccessInfo.ts
+++ next-mongo-app/pages/api/getAccessInfo.ts
@@ -1,28 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import AccessInfo from "../../models/accessInfo"; // Import your Mongoose model
-
-class GetAccessInfo {
-  constructor() {
-    this.handle = this.handle.bind(this); // Bind the handle method to the current instance
-  }
-  async handle(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "GET") {
-      try {
-        // Fetch user info sorted by timestamp in descending order
-        const userInfos = await AccessInfo.find().sort({ timestamp: -1 });
-
-        res.status(200).json(userInfos);
-      } catch (error) {
-        res
-          .status(500)
-          .json({ error: "Failed to fetch access info", err: error });
-      }
-    } else {
-      res.status(405).json({ error: "Method not allowed" });
+import connectDB from "../../lib/mongodb";
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const dbconnection = await connectDB();
+      // Fetch user info sorted by timestamp in descending order
+      const userInfos = await AccessInfo.find().sort({ timestamp: -1 });
+      res.status(200).json(userInfos);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to fetch access info", err: error });
     }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
-
-const handler = new GetAccessInfo();
-
-export default handler.handle;
--- next-mongo-app/pages/list.tsx
+++ next-mongo-app/pages/list.tsx
@@ -1,9 +1,7 @@
-// pages/list.tsx
 import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-// Define an interface to match the structure of your user access information
 interface UserInfo {
   timestamp: string;
   userAgent: string;
@@ -14,7 +12,6 @@ const List = () => {
   const [userInfos, setUserInfos] = useState<UserInfo[]>([]);
 
   useEffect(() => {
-    // Fetch user access info from the API route when the component mounts
     fetch("/api/getAccessInfo")
       .then((response) => response.json())
       .then((data) => {
@@ -35,10 +32,17 @@ const List = () => {
         <Link href="/">Back to Home</Link>
         {userInfos.map((userInfo, index) => (
           <div key={index} className="user-info-card">
-            <h2>Timestamp: {userInfo.timestamp}</h2>
+            <h2>Timestamp: {new Date(userInfo.timestamp).toLocaleString()}</h2>
             <p>User Agent: {userInfo.userAgent}</p>
             <p>
-              Location: <a href={userInfo.mapLink}>{userInfo.mapLink}</a>
+              Location:{" "}
+              <a
+                href={userInfo.mapLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View on Google Maps
+              </a>
             </p>
           </div>
         ))}
@@ -47,6 +51,8 @@ const List = () => {
             border: 1px solid #ccc;
             padding: 16px;
             margin-bottom: 16px;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
           }
         `}</style>
       </main>
